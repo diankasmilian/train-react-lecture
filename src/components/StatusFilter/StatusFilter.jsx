52c9958e6af451c06d6ee02817a8f7085c98b9a2
Diana Smilian <dianka.kulakevich@gmail.com>
2023-07-26 12:16:02 +0300

    finish

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,7 +1,7 @@
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { setStatusFilter } from '../../redux/actions';
+import { setStatusFilter } from "redux/filtersSlice";
 import { statusFilters } from '../../redux/constants';
 import { getStatusFilter } from 'redux/selectors';
 
